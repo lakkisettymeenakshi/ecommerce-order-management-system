88321fed0a0a415cc2243a1592ef5e2e6f1c9e26
lakkisetty meenakshi <lakkisettymeenakshi.1501@gmail.com>
2026-05-24 18:24:51 -0500

    Cleaned project structure

--- src/main/java/com/ecommerce/backend/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.backend.repository;
-
-import com.ecommerce.backend.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository
-        extends JpaRepository<Product, Long> {
-}
